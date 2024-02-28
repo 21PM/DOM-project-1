f605132c5b7ee5753e79001957725c9f1bc22ce0
21PM <parasmore33@gmail.com>
2024-02-28 20:40:26 +0530

    commited dom1

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var divId = document.getElementById("container");
+console.log("hello");
+divId.style.height =  "200px";                                                                                                                                                                                                                                                                                                                                                                                                                    ;
+divId.style.width = "300px";
+divId.style.backgroundColor = "yellow";
+divId.style.margin = "20px";
+divId.style.padding = "10px";
+divId.style.fontSize = "18px";
+divId.style.fontWeight = "bold";
+divId.style.color =  "blue";                                                                                                                                                                                                                                                                                                                                                                                                                    ;
+
+
+
